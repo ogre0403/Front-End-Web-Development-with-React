607dd71b2055ed54b0c6ef25343e6de13d335dfb
Jimmy Chuang <ogre0403@gmail.com>
2020-06-27 15:09:30 +0800

    DishDetailComponent use functional componemt

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,57 +1,60 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap'
 
-class DishDetail extends Component {
-  renderDish(dish) {
-    if (dish != null) {
-      const comment = dish.comments.map((c) => {
-        return (
-          <>
-            <CardText>{c.comment}</CardText>
-            <CardText>
-              {`--- ${c.author}, 
+// function RenderDish({ dish }) {
+// or
+// const RenderDish = ({ dish }) => {
+const RenderDish = ({ dish }) => {
+  if (dish != null) {
+    const comment = dish.comments.map((c) => {
+      return (
+        <>
+          <CardText>{c.comment}</CardText>
+          <CardText>
+            {`--- ${c.author}, 
             ${new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(
               new Date(Date.parse(c.date))
             )}`}
-            </CardText>
-          </>
-        )
-      })
-
-      return (
-        <>
-          <div className="col-12 col-md-5 m-1">
-            <Card>
-              <CardImg width="100%" src={dish.image} alt={dish.name} />
-              <CardBody>
-                <CardTitle>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
-              </CardBody>
-            </Card>
-          </div>
-          <div className="col-12 col-md-5 m-1">
-            <Card className="border-0">
-              <CardBody>
-                <CardTitle>Comment</CardTitle>
-                {comment}
-              </CardBody>
-            </Card>
-          </div>
+          </CardText>
         </>
       )
-    } else {
-      return <div></div>
-    }
-  }
+    })
 
-  render() {
     return (
-      <div className="container">
-        <div className="row">{this.renderDish(this.props.dish)}</div>
-      </div>
+      <>
+        <div className="col-12 col-md-5 m-1">
+          <Card>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardBody>
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+            </CardBody>
+          </Card>
+        </div>
+        <div className="col-12 col-md-5 m-1">
+          <Card className="border-0">
+            <CardBody>
+              <CardTitle>Comment</CardTitle>
+              {comment}
+            </CardBody>
+          </Card>
+        </div>
+      </>
     )
+  } else {
+    return <div></div>
   }
+}
+
+const DishDetail = (props) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <RenderDish dish={props.dish} />
+      </div>
+    </div>
+  )
 }
 
 export default DishDetail
