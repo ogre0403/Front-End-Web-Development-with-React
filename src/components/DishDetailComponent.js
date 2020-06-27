c317c17ef005532ab1ddd231cd0f7ce0f4185dd3
Jimmy Chuang <ogre0403@gmail.com>
2020-06-27 21:18:37 +0800

    spa-2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap'
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap'
+import { Link } from 'react-router-dom'
 
 // function RenderDish({ dish }) {
 // or
 // const RenderDish = ({ dish }) => {
-const RenderDish = ({ dish }) => {
+const RenderDish = ({ dish, comments }) => {
   if (dish != null) {
-    const comment = dish.comments.map((c) => {
+    const comment = comments.map((c) => {
       return (
         <>
           <CardText>{c.comment}</CardText>
@@ -51,7 +52,19 @@ const DishDetail = (props) => {
   return (
     <div className="container">
       <div className="row">
-        <RenderDish dish={props.dish} />
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/menu">Menu</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>{props.dish.name}</h3>
+          <hr />
+        </div>
+      </div>
+      <div className="row">
+        <RenderDish dish={props.dish} comments={props.comments} />
       </div>
     </div>
   )
