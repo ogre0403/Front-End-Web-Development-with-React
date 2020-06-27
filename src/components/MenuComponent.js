f5cba88a98b0b4a50973ae06cedbea8e70ae3e3f
Jimmy Chuang <ogre0403@gmail.com>
2020-06-27 14:30:21 +0800

    MenuComponent use functional componemt

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,27 +1,34 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap'
 
-class Menu extends Component {
-  render() {
-    const menu = this.props.dishes.map((dish) => {
-      return (
-        <div className="col-12 col-md-5 m-1">
-          <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
-            <CardImgOverlay>
-              <CardTitle>{dish.name}</CardTitle>
-            </CardImgOverlay>
-          </Card>
-        </div>
-      )
-    })
+// 如果是整個props, 不用加 { }
+// function RenderMenuItem({ dish, onClick }) {
+const RenderMenuItem = ({ dish, onClick }) => {
+  return (
+    <Card key={dish.id} onClick={() => onClick(dish.id)}>
+      <CardImg width="100%" src={dish.image} alt={dish.name} />
+      <CardImgOverlay>
+        <CardTitle>{dish.name}</CardTitle>
+      </CardImgOverlay>
+    </Card>
+  )
+}
 
+// 如果參數是props中的幾個，要加 { }
+const Menu = (props) => {
+  const menu = props.dishes.map((dish) => {
     return (
-      <div className="container">
-        <div className="row">{menu}</div>
+      <div className="col-12 col-md-5 m-1">
+        <RenderMenuItem dish={dish} onClick={props.onClick} />
       </div>
     )
-  }
+  })
+
+  return (
+    <div className="container">
+      <div className="row">{menu}</div>
+    </div>
+  )
 }
 
 export default Menu
