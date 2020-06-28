25d67b196d8bc02b0544b02e988ccf0804e129c5
Jimmy Chuang <ogre0403@gmail.com>
2020-06-28 15:22:37 +0800

    redux

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import Main from './components/MainComponent'
 import DISHES from './shared/dishes'
 import { BrowserRouter } from 'react-router-dom'
+import { ConfigureStore } from './redux/configureStore'
+import { Provider } from 'react-redux'
+
+const store = ConfigureStore()
 
 class App extends Component {
   constructor(props) {
@@ -13,11 +17,13 @@ class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Main />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     )
   }
 }
